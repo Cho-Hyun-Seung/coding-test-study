9a5dff80067c6c17da4c6025cada3475a9ceaaec
Cho-Hyun-Seung <105296338+Cho-Hyun-Seung@users.noreply.github.com>
2025-01-23 23:17:47 +0900

    [level 0] Title: 더 크게 합치기, Time: 33.71 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int a, int b) {
+        int case1 = Integer.parseInt(String.valueOf(a) + String.valueOf(b));
+        int case2 = Integer.parseInt(String.valueOf(b+ String.valueOf(a)));
+        int answer = case1 > case2 ? case1 : case2;
+        return answer;
+    }
+}
