cad896fb318e0ed2908f710addea4834bcde78d7
Cho-Hyun-Seung <105296338+Cho-Hyun-Seung@users.noreply.github.com>
2025-01-28 21:32:32 +0900

    [level 0] Title: 글자 지우기, Time: 2.17 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,15 +1,31 @@
-import java.util.*;
-import java.util.stream.Collectors;
-
 class Solution {
     public String solution(String my_string, int[] indices) {
         String answer = "";
-        List<Integer> indices_list = Arrays.stream(indices).boxed().collect(Collectors.toList());
-        for(int i = 0; i < my_string.length(); i++){
-            if(!indices_list.contains(i)){
-                answer += my_string.charAt(i);
-            }
+        String[] str_arr = my_string.split("");
+        
+        for(int num: indices){
+            str_arr[num] = "";
+        }
+        
+        for(String str: str_arr){
+            answer += str;
         }
         return answer;
     }
 }
+
+// import java.util.*;
+// import java.util.stream.Collectors;
+
+// class Solution {
+//     public String solution(String my_string, int[] indices) {
+//         String answer = "";
+//         List<Integer> indices_list = Arrays.stream(indices).boxed().collect(Collectors.toList());
+//         for(int i = 0; i < my_string.length(); i++){
+//             if(!indices_list.contains(i)){
+//                 answer += my_string.charAt(i);
+//             }
+//         }
+//         return answer;
+//     }
+// }
