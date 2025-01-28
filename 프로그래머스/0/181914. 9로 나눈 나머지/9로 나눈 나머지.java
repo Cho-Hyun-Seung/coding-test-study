6c58d1b8ef35b8134c187e993b1b71a736518192
Cho-Hyun-Seung <105296338+Cho-Hyun-Seung@users.noreply.github.com>
2025-01-28 14:58:12 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 12.81 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -1,11 +1,22 @@
+// class Solution {
+//     public int solution(String number) {
+//         int answer = 0;
+//         int sum = 0;
+//         for(char ch: number.toCharArray()){
+//             sum += Character.getNumericValue(ch);
+//         }
+//         answer = sum % 9;
+//         return answer;
+//     }
+// }
+
 class Solution {
     public int solution(String number) {
-        int answer = 0;
-        int sum = 0;
-        for(char ch: number.toCharArray()){
-            sum += Character.getNumericValue(ch);
-        }
-        answer = sum % 9;
+        int answer = number.chars().map(v -> Character.getNumericValue(v)).sum() % 9;
         return answer;
     }
 }
+    
+    
+    
+    
