0cf42a9b2fa04990774d99b47f03f77fb5138546
Cho-Hyun-Seung <105296338+Cho-Hyun-Seung@users.noreply.github.com>
2025-02-19 18:51:29 +0900

    [Bronze III] Title: 알람 시계, Time: 196 ms, Memory: 18536 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int hour = sc.nextInt();
+        int minute = sc.nextInt() - 45;
+        // hour 바꿔야 하는 경우
+        if(minute < 0){
+            hour -=1;
+            if(hour == -1){
+                hour = 23;
+            }
+            minute = 60 + minute;
+        }
+        System.out.println(hour+" "+minute);
+    }
+}
