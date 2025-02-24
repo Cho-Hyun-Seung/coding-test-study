e44cffdb852738af46859a0517459f3423cef24e
Cho-Hyun-Seung <105296338+Cho-Hyun-Seung@users.noreply.github.com>
2025-02-24 21:56:11 +0900

    [Bronze II] Title: 소수 찾기, Time: 104 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    private static boolean isDecimal(int num){
+        if(num <= 1){
+            return false;
+        }
+        for(int i = 2; i < num; i++){
+            if( num % i == 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int testCase = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int result = 0;
+
+        while(st.hasMoreTokens()){
+            int num = Integer.parseInt(st.nextToken());
+            if(isDecimal(num)) {
+                result +=1;
+            }
+        }
+        sb.append(result);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
