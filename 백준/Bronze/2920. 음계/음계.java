91b9be51ff837709d0c68a72f489b53d7c7beef4
Cho-Hyun-Seung <105296338+Cho-Hyun-Seung@users.noreply.github.com>
2025-02-25 17:02:42 +0900

    [Bronze II] Title: 음계, Time: 104 ms, Memory: 14076 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.java
+++ 백준/Bronze/2920. 음계/음계.java
@@ -1,56 +1,28 @@
-/* 
-다장조는 c d e f g a b C, 총 8개 음으로 이루어져있다. 
-이 문제에서 8개 음은 다음과 같이 숫자로 바꾸어 표현한다. 
-c는 1로, d는 2로, ..., C를 8로 바꾼다.
+import java.util.*;
+import java.io.*;
 
-1부터 8까지 차례대로 연주한다면 ascending, 
-8부터 1까지 차례대로 연주한다면 descending, 
-둘 다 아니라면 mixed 이다.
-*/
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-import java.util.Scanner;
-
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        // 1번 번호로 처음 필터링
-        int prevNum = sc.nextInt();
-
-        if(prevNum == 1){
-            while(sc.hasNext()){
-                int nextNum = sc.nextInt();
-                if(isAsc(prevNum, nextNum)){
-                    prevNum = nextNum;
-                }else{
-                    System.out.println("mixed");
-                    return;
-                }
+        boolean isAscending = false;
+        boolean isDescending = false;
+        int pastNum = Integer.parseInt(st.nextToken());
+        if (pastNum == 8) isDescending = true;
+        if (pastNum == 1) isAscending = true;
+        while ((isAscending || isDescending) && st.hasMoreTokens()) {
+            int num = Integer.parseInt(st.nextToken());
+            if (isAscending && pastNum + 1 != num) {
+                isAscending = false;
+                break;
             }
-            System.out.println("ascending");
-        }else if (prevNum == 8){
-            while(sc.hasNext()){
-                int nextNum = sc.nextInt();
-                if(isDesc(prevNum, nextNum)){
-                    prevNum = nextNum;
-                }else{
-                    System.out.println("mixed");
-                    return;
-                }
+            if (isDescending && pastNum - 1 != num) {
+                isDescending = false;
+                break;
             }
-            System.out.println("descending");
-        }else{
-            System.out.println("mixed");
+            pastNum = num;
         }
-
-    }
-
-    public static boolean isAsc(int prevNum, int nextNum){
-        return prevNum + 1 == nextNum;
-    }
-
-    public static boolean isDesc(int prevNum, int nextNum){
-        return prevNum - 1 == nextNum;
+        System.out.println(isAscending ? "ascending" : isDescending ? "descending" : "mixed");
     }
 }
-
-
