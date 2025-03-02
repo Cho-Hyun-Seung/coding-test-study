7f50720c86ee73a5ffccf56be38a3b6c17c0cf8d
Cho-Hyun-Seung <105296338+Cho-Hyun-Seung@users.noreply.github.com>
2025-03-03 01:10:41 +0900

    [Silver V] Title: 덩치, Time: 100 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int[][] people = new int[num][2];
+        for(int i = 0; i < num; i++){
+            st = new StringTokenizer(br.readLine());
+            people[i][0] = Integer.parseInt(st.nextToken());
+            people[i][1] = Integer.parseInt(st.nextToken());
+        }
+        for (int[] person : people) {
+            int rank = 1;
+            for (int[] targetPerson : people) {
+                if (targetPerson[0] > person[0] && targetPerson[1] > person[1]) {
+                    rank += 1;
+                }
+            }
+            sb.append(rank).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
