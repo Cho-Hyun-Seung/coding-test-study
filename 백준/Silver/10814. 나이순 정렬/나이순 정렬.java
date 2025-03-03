104315226171a1dfff141f341853517c7beca783
Cho-Hyun-Seung <105296338+Cho-Hyun-Seung@users.noreply.github.com>
2025-03-03 18:38:45 +0900

    [Silver V] Title: 나이순 정렬, Time: 580 ms, Memory: 46240 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static class User {
+        int age;
+        String name;
+
+        User(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int length = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        List<User> users = new ArrayList<>();
+
+        while (length-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int age = Integer.parseInt(st.nextToken());
+            String name = st.nextToken();
+            users.add(new User(age, name));
+        }
+
+        users.sort((a, b) -> a.age - b.age);
+
+        for (User user : users) {
+            sb.append(user.age).append(" ").append(user.name).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
