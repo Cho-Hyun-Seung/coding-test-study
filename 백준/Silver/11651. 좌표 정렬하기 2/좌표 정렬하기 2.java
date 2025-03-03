2d5f8829f6edb273b8cbc11e13f487a9ea0de837
Cho-Hyun-Seung <105296338+Cho-Hyun-Seung@users.noreply.github.com>
2025-03-04 00:31:55 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 644 ms, Memory: 50744 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int length = Integer.parseInt(br.readLine());
+        int[][] coordArr = new int[length][2];
+
+        for(int i = 0; i < length; i ++){
+            st = new StringTokenizer(br.readLine());
+            coordArr[i][0] = Integer.parseInt(st.nextToken());
+            coordArr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(coordArr,((a, b) -> {
+            if(a[1] == b[1]){
+                return a[0] - b[0];
+            }
+            return a[1] - b[1];
+        }));
+
+        for(int[] coord : coordArr){
+            sb.append(coord[0]).append(" ").append(coord[1]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
