033b870b36e631ef43b8774e3ee581112617d3cc
Cho-Hyun-Seung <105296338+Cho-Hyun-Seung@users.noreply.github.com>
2025-03-05 06:21:03 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 104 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -1,49 +1,42 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    public static char[][] board;
-    public static int min = 64;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] num = br.readLine().split(" ");
+        int n = Integer.parseInt(num[0]);
+        int m = Integer.parseInt(num[1]);
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        board = new char[n][m];
-
+        char[][] board = new char[n][m];
         for (int i = 0; i < n; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < m; j++) {
-                board[i][j] = line.charAt(j);
-            }
+            board[i] = br.readLine().toCharArray();
         }
+
+        int minPaint = Integer.MAX_VALUE;
 
         for (int i = 0; i <= n - 8; i++) {
             for (int j = 0; j <= m - 8; j++) {
-                reDraw(i, j);
+                minPaint = Math.min(minPaint, repaintCount(board, i, j));
             }
         }
-        System.out.println(min);
+
+        System.out.println(minPaint);
     }
 
-    public static void reDraw(int startN, int startM) {
-        int count = 0; 
-        char firstColor = board[startN][startM]; 
+    private static int repaintCount(char[][] board, int startX, int startY) {
+        int countWStart = 0;
+        int countBStart = 0;
 
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                if (board[startN + i][startM + j] != firstColor) {
-                    count++;
-                }
-                firstColor = (firstColor == 'W') ? 'B' : 'W'; 
+                char caseW = ((i + j) % 2 == 0) ? 'W' : 'B'; 
+                char caseB = ((i + j) % 2 == 0) ? 'B' : 'W';
+                
+                if (board[startX + i][startY + j] != caseW) countWStart++;
+                if (board[startX + i][startY + j] != caseB) countBStart++;
             }
-            firstColor = (firstColor == 'W') ? 'B' : 'W'; 
         }
 
-        count = Math.min(count, 64 - count);
-
-        min = Math.min(min, count);
+        return Math.min(countWStart, countBStart);
     }
 }
