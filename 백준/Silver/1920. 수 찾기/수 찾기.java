e375263ec160e01ea975e2503296611dcbb71164
Cho-Hyun-Seung <105296338+Cho-Hyun-Seung@users.noreply.github.com>
2025-03-05 06:06:19 +0900

    [Silver IV] Title: 수 찾기, Time: 544 ms, Memory: 49112 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Set<Integer> numSet = new HashSet<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            numSet.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        while (st.hasMoreTokens()) {
+            int num = Integer.parseInt(st.nextToken());
+            sb.append(numSet.contains(num) ? 1 : 0).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
