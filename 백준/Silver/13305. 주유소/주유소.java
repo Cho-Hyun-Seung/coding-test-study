dbe8350ef4f92450bc62a86b702922c6083a28c0
Cho-Hyun-Seung <105296338+Cho-Hyun-Seung@users.noreply.github.com>
2025-03-12 15:50:03 +0900

    [Silver III] Title: 주유소, Time: 396 ms, Memory: 35592 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -8,10 +8,10 @@ public class Main{
         
         int length = Integer.parseInt(br.readLine());
         
-        int[] roads = new int[length - 1];
+        long[] roads = new long[length - 1];
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < roads.length; i++){
-            roads[i] = Integer.parseInt(st.nextToken());
+            roads[i] = Long.parseLong(st.nextToken());
         }
         
         int[] oils = new int[length];
@@ -20,26 +20,15 @@ public class Main{
             oils[i] = Integer.parseInt(st.nextToken());
         }
         
-        int nowOils = 0;    // 현재 오일 잔여량
-        int cost = 0;
+        long nowOilCost = oils[0];    // 현재 비용
+        long cost = 0;
         
         for(int i = 0; i < roads.length; i++){
-            // 최소 요구치 충족 여부 확인
-            if(roads[i] > nowOils){
-                nowOils +=roads[i];
-                cost += roads[i] * oils[i];
-            }
             
-            // 남은 주유소들보다 지금이 쌀 경우
-            for(int j = i + 1 ; j <roads.length; j++){
-                if(oils[i] < oils[j]){
-                    nowOils += roads[j];
-                    cost += oils[i] * roads[j];
-                }else{
-                    break;
-                }
+            if(oils[i] < nowOilCost){
+                nowOilCost = oils[i];
             }
-            nowOils -= roads[i];
+            cost += roads[i] * nowOilCost;
         }
         System.out.println(cost);
     }
