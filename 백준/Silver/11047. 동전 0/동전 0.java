d10dc812b756f597d7bf5c96b234ea7b99e0778f
Cho-Hyun-Seung <105296338+Cho-Hyun-Seung@users.noreply.github.com>
2025-03-15 15:59:06 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static int[] coins;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());    // 동전의 종류
+        int k = Integer.parseInt(st.nextToken());    // 가치 합
+        int answer = 0;
+        coins = new int[n];
+        
+        for(int i = 0; i< n ; i++){
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+        
+        for(int i = n-1; i >= 0; i--){
+            if(k == 0) break;
+            answer += k / coins[i];
+            k %= coins[i]; 
+        }
+        
+        System.out.println(answer);
+    }
+}
