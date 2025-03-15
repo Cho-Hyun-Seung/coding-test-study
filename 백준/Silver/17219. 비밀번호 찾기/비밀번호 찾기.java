bc6ab9d010d398a1c10151468c93e1d7f0bf7cfd
Cho-Hyun-Seung <105296338+Cho-Hyun-Seung@users.noreply.github.com>
2025-03-15 17:52:13 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 552 ms, Memory: 57900 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -10,15 +10,15 @@ public class Main{
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         
-        HashMap<String, String> hashset = new HashMap<>();
+        Map<String, String> hashmap = new HashMap<>();
         
         for(int i = 0 ; i < n ; i++){
             st = new StringTokenizer(br.readLine());
-            hashset.put(st.nextToken(), st.nextToken());
+            hashmap.put(st.nextToken(), st.nextToken());
         }
         
         for(int i = 0; i < m ; i++){
-            sb.append(hashset.get(br.readLine())).append("\n");
+            sb.append(hashmap.get(br.readLine())).append("\n");
         }
         
         System.out.println(sb);
