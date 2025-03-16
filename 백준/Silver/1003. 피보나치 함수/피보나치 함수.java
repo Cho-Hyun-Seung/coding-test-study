eef2505d8057f211086ac7b449220f89fd499afa
Cho-Hyun-Seung <105296338+Cho-Hyun-Seung@users.noreply.github.com>
2025-03-16 17:57:06 +0900

    [Silver III] Title: 피보나치 함수, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[][] dp;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testCase = Integer.parseInt(br.readLine());
+
+        int maxNum = 0;
+        int[] inputs = new int[testCase];
+
+        for (int i = 0; i < testCase; i++) {
+            inputs[i] = Integer.parseInt(br.readLine());
+            maxNum = Math.max(maxNum, inputs[i]);
+        }
+
+        dp = new int[Math.max(2, maxNum + 1)][2]; 
+        dp[0] = new int[]{1, 0};
+        dp[1] = new int[]{0, 1};
+
+        for (int i = 2; i <= maxNum; i++) {
+            dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+            dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+        }
+
+        for (int num : inputs) {
+            sb.append(dp[num][0]).append(" ").append(dp[num][1]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
