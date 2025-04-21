60c04cb41624bbcf717b7bc041f5ffe09234e553
Cho-Hyun-Seung <105296338+Cho-Hyun-Seung@users.noreply.github.com>
2025-04-21 10:26:29 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 91.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        for(int num: arr){
+            answer += num;
+        }
+        
+        answer /= arr.length;
+        return answer;
+    }
+}
