182465c21299bb054beda2a8d9ccbcd0957998cb
Cho-Hyun-Seung <105296338+Cho-Hyun-Seung@users.noreply.github.com>
2025-06-13 18:17:51 +0900

    [Bronze II] Title: 숫자의 개수, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,26 +1,30 @@
-import java.util.Scanner;
-/*
-    세 개의 자연수 A, B, C가 주어질 때 A × B × C를 계산한 결과에 
-    0부터 9까지 각각의 숫자가 몇 번씩 쓰였는지를 구하는 프로그램을 작성하시오.
-*/
+import java.util.*;
+import java.io.*;
+
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-
-        int[] arr = new int[10]; // count 수를 위한 배열
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int num = a * b * c;
-        String strNum = "" + num;
-
-        for(int i = 0; i < strNum.length(); i++){
-            int idx = strNum.charAt(i) - '0';
-            arr[idx] +=1;
+    private static int A,B,C;
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = 
+            new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int[] arr = new int[10];
+        
+        A = Integer.parseInt(br.readLine());
+        B = Integer.parseInt(br.readLine());
+        C = Integer.parseInt(br.readLine());
+        
+        int data = A*B*C;
+        
+        String str = data + "";
+        
+        for(char ch: str.toCharArray()){
+            arr[ch-48] +=1;
         }
-
-        for(int i = 0; i < arr.length; i ++){
-            System.out.println(arr[i]);
+        
+        for(int num: arr){
+            sb.append(num).append("\n");
         }
+        System.out.println(sb.toString());
     }
 }
