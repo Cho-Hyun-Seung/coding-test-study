4fc04af45f96965ca1aa1d1f92749f5d4bf6a2cd
Cho-Hyun-Seung <105296338+Cho-Hyun-Seung@users.noreply.github.com>
2025-06-19 01:10:13 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 45596 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -2,24 +2,28 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
-    public static void main(String[] args) throws IOException {
+    private static int N;
+    private static Queue<Integer> que;
+    
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(br.readLine());
-        Queue<Integer> cards = new LinkedList<>();
-
-        for (int i = 1; i <= num; i++) {
-            cards.offer(i);
+        N = Integer.parseInt(br.readLine());
+        que = new LinkedList<>();
+        
+        // 1. 값 넣어 주기
+        for(int i = 1; i <= N; i++){
+            que.offer(i);
         }
-        boolean dropCard = true;
-        while (cards.size() > 1) {
-            int targetCard = cards.poll();
-            if (dropCard) {
-                dropCard = false;
-            } else {
-                cards.offer(targetCard);
-                dropCard = true;
-            }
+        
+        // 2. 큐 사이즈가 1이 될 때 까지 반복
+        while(que.size() >1){
+            // 1. 하나 빼기
+            que.poll();
+            // 2. 하나 빼서 넣어주기
+            que.offer(que.poll());
         }
-        System.out.println(cards.poll());
+        
+        System.out.println(que.poll());
+        
     }
 }
