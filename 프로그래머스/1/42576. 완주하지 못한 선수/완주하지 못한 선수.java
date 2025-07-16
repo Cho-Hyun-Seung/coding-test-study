9ebd6d08e677769d5dfcf451b166ef19cde8179c
Cho-Hyun-Seung <105296338+Cho-Hyun-Seung@users.noreply.github.com>
2025-07-16 21:33:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 83.72 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -5,19 +5,21 @@ class Solution {
         String answer = "";
         HashMap<String, Integer> map = new HashMap<>();
         
-        // 값 입력
-        for(String name: completion){
+        for(String name: participant){
             map.put(name, map.getOrDefault(name, 0) + 1);
         }
         
-        // 값 찾기
-        for(String name : participant) {
-            if(map.get(name) == null || map.get(name) == 0){
-                answer = name;
-                break;
-            }
+        for(String name: completion){
             map.put(name, map.get(name) - 1);
         }
+        
+        for(String name:participant){
+            int num = map.get(name);
+            if(num > 0){
+                answer = name;
+            }
+        }
+        
         return answer;
     }
 }
