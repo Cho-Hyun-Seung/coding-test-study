279b5fa687d431959f0dfc380d9dc676ed72f4ca
Cho-Hyun-Seung <105296338+Cho-Hyun-Seung@users.noreply.github.com>
2025-09-18 23:39:49 +0900

    [level 2] Title: 귤 고르기, Time: 105.52 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,32 +1,25 @@
 import java.util.*;
-import java.util.stream.*;
-/*
-    - k개를 골라 상자 하나에 담아 판매
-    - 서로 다른 종류의 수 최소화 (hash?)
-*/
+
 class Solution {
-    private static Map<Integer, Integer> map = new HashMap<>();
-    
+    private static Map<Integer, Integer> map;
     public int solution(int k, int[] tangerine) {
         int answer = 0;
+        map = new HashMap<>();
         
-        for(int i = 0; i < tangerine.length; i++){
-            map.put(tangerine[i], map.getOrDefault(tangerine[i], 0) + 1);
+        for(int tang: tangerine){
+            map.put(tang, map.getOrDefault(tang, 0) + 1);
         }
         
-        // 키셋을 개수 기준으로 정렬 시키기
-        List<Integer> keySet = map.keySet()
-            .stream()
-            .sorted((a,b) -> map.get(b) - map.get(a))
-            .collect(Collectors.toList());
+        List<Integer> list = new ArrayList<>(map.keySet());
+        Collections.sort(list, (a, b) -> map.get(b) - map.get(a));
         
-        for(int key: keySet){
-            if(k > 0){
-                k -= map.get(key);
-                answer += 1;
-            }else{
-                break;
+        int selected = 0;
+        for(int num : list){
+            if(selected >= k){
+               break; 
             }
+            selected += map.get(num);
+            answer += 1;
         }
         return answer;
     }
