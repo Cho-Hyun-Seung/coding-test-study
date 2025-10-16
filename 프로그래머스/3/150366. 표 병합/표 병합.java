2fbb68e0a265bec4162112dca393dd92e4fe464f
Cho-Hyun-Seung <105296338+Cho-Hyun-Seung@users.noreply.github.com>
2025-10-17 00:34:43 +0900

    [level 3] Title: 표 병합, Time: 18.87 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/3/150366. 표 병합/표 병합.java
+++ 프로그래머스/3/150366. 표 병합/표 병합.java
@@ -1,151 +1,168 @@
 import java.util.*;
-/*
-    - update r c value : r,c 위치의 셀 선택하여 셀 값 변경
-    - update value1 value2 : value1값 모두 선택해 value2로 변경
-    - merge r1 c1 r2 c2 : (r1, c1)위치의 셀과 (r2, c2)위치의 셀 병합
-        - 두 셀 중 한 셀이 값을 가진경우 병한된 셀은 그값을 가지게 됨
-        - 둘다 값을 가진 경우 r1, c1 위치의 셀 값을 가지게 됨
-        - 이후 두 셀 중 어느 위치를 선택해도 병합된 셀로 접근
-    - unmerge r c : 병합 해제
-    - print r c : 셀의 값 출력, 빈 경우 EMPTY 출력
-    
-    부모 요소로 가기? 유니온 파인드?
-*/
 
 
 class Solution {
-    static final int N = 50;
-    static final int rc = 5050;
+    private int N = 50;
+    private int rc = 5050;
+    private int[] union;
+    private String[] value;
     
-    static int[] union = new int[rc + 1];
-    static String[] value = new String[rc + 1];
+    private int find(int x){
+        if(union[x] == x) return x;
+        return union[x] = find(union[x]);
+    }
+    
+    private int toCoord(int r, int c){
+        return r * 100 + c;
+    }
     
     public String[] solution(String[] commands) {
         List<String> answer = new ArrayList<>();
-
-        for (int r = 1; r <= 50; r++) {
-            for (int c = 1; c <= 50; c++) {
+        
+        union = new int[rc + 1];
+        value = new String[rc + 1];
+        
+        // 유니온 파인드 준비하기!
+        for(int r = 1; r <= 50; r++){
+            for(int c = 1; c <= 50; c++){
                 int idx = toCoord(r, c);
                 union[idx] = idx;
                 value[idx] = null;
             }
         }
-
-        for (String command : commands) {
-            String[] arr = command.split(" ");
+        
+        StringTokenizer st;
+        for(String command: commands){
+            String[] cmdArr = command.split(" ");
             
-            switch (arr[0]) {
+            switch(cmdArr[0]){
                 case "UPDATE":
-                    if (arr.length == 4) {
-                        update(arr);
-                    } else {
-                        updateAll(arr);
-                    }
+                    if(cmdArr.length == 4) updateOne(cmdArr);
+                    else if(cmdArr.length == 3) updateAll(cmdArr);
                     break;
-                    
                 case "MERGE":
-                    merge(arr);
+                    merge(cmdArr);
                     break;
-                    
                 case "UNMERGE":
-                    unmerge(arr);
+                    unmerge(cmdArr);
                     break;
-                    
                 case "PRINT":
-                    answer.add(print(arr));
+                    String v = print(cmdArr);
+                    answer.add((v == null) ? "EMPTY" : v);
                     break;
             }
         }
-        return answer.toArray(new String[0]);
-    }
-    
-    static int find(int x){
-        if(union[x] == x) return x;
-        return union[x] = find(union[x]);
-    }
-    
-    static int toCoord(int r, int c){
-        return r*100 + c;
-    }
-    
-    static void update(String[] arr){
-        int r = Integer.parseInt(arr[1]);
-        int c = Integer.parseInt(arr[2]);
-        String v = arr[3];
         
-        int coord = toCoord(r, c);
-        
-        // 병합된 경우도 있으니 루트에 값 넣어 주기
-        int rootCoord = find(coord);
-        value[rootCoord] = v;
+        return answer.toArray(String[]::new);
     }
     
-    static void updateAll(String[] arr){
-        String v1 = arr[1];
-        String v2 = arr[2];
+    // r, c의 위치를 value로 update
+    // TODO 병합 여부 체크하기!! - 완료
+    private void updateOne(String[] cmdArr){
+        int r = Integer.parseInt(cmdArr[1]);
+        int c = Integer.parseInt(cmdArr[2]);
+        String v = cmdArr[3];
         
-        // 루트 값만 돌면서 확인해 주기!
-        for (int i = 101; i <= rc; i++) {
-            if (union[i] == i && v1.equals(value[i])) {
-                value[i] = v2;
-            }
-        }
-    }
-    
-    static void merge(String[] arr) {
-        int r1 = Integer.parseInt(arr[1]);
-        int c1 = Integer.parseInt(arr[2]);
-        int r2 = Integer.parseInt(arr[3]);
-        int c2 = Integer.parseInt(arr[4]);
-
-        int i1 = toCoord(r1, c1);
-        int i2 = toCoord(r2, c2);
-
-        int p1 = find(i1);
-        int p2 = find(i2);
-        if (p1 == p2) return;
-
-        String v1 = value[p1];
-        String v2 = value[p2];
-        String keep = (v1 != null) ? v1 : v2;
-
-        union[p2] = p1;
-        value[p2] = null;
-        value[p1] = keep;
-    }
-
-    static void unmerge(String[] arr) {
-        int r = Integer.parseInt(arr[1]);
-        int c = Integer.parseInt(arr[2]);
         int idx = toCoord(r, c);
-
         int root = find(idx);
-        String keep = value[root];
-
-        List<Integer> members = new ArrayList<>();
-        for (int rr = 1; rr <= N; rr++) {
-            for (int cc = 1; cc <= N; cc++) {
-                int now = toCoord(rr, cc);
-                if (find(now) == root) {
-                    members.add(now);
+        
+        value[root] = v;
+    }
+    
+    // value1을 값으로 가지고 있는 모든 셀을 value2로 바꿈
+    // TODO 병합 여부 체크하기!! - 완료
+    private void updateAll(String[] cmdArr){
+        String v1 = cmdArr[1];
+        String v2 = cmdArr[2];
+        
+        for(int i = 1; i <= 50; i++){
+            for(int j = 1; j <= 50; j++){
+                int cur = toCoord(i, j);
+                if(union[cur] == cur && v1.equals(value[cur])){
+                    value[cur] = cmdArr[2];
                 }
             }
         }
-
-        for (int m : members) {
+    }
+    
+    /*
+        - r1,c1 과 r2, c2 위치의 셀을 선택하여 병합
+        - 두 위치의 셀이 같을 경우 무시
+        - 한 셀이 값을 가진 경우 병합된 셀은 그 값을 가짐
+        - 두 셀 모두 값을 가진 경우 병합된 셀은 r1, c1 위치의 값을 가짐
+        - 이후 r1, c1 r2, c2 어느 위치를 선택해도 병합된 셀로 접근
+    */
+    private void merge(String[] cmdArr){
+        int r1 = Integer.parseInt(cmdArr[1]);
+        int c1 = Integer.parseInt(cmdArr[2]);
+        int r2 = Integer.parseInt(cmdArr[3]);
+        int c2 = Integer.parseInt(cmdArr[4]);
+        
+        
+        
+        int idx1 = toCoord(r1, c1);
+        int idx2 = toCoord(r2, c2);
+        
+        if(idx1 == idx2) return;
+        
+        int root1 = find(idx1);
+        int root2 = find(idx2);
+        
+        String value1 = value[root1];
+        String value2 = value[root2];
+        
+        String temp = (value1 != null) ? value1 : value2;
+        
+        union[root2] = union[root1];
+        // 부모에서 value를 찾을 거니 null 처리!
+        value[root2] = null;
+        value[root1] = temp;
+        
+        return;
+    }
+    
+    // r, c 위치의 셀을 선태갛여 해당 셀의 모든 병합 해제
+    // 값을 가지고 있을 경우 r, c위치의 셀이 그 값을 가지게 됨
+    private void unmerge(String[] cmdArr){
+        int r = Integer.parseInt(cmdArr[1]);
+        int c = Integer.parseInt(cmdArr[2]);
+        
+        int idx = toCoord(r, c);
+        
+        int root = find(idx);
+        String temp = value[root];
+        
+        List<Integer> member = new ArrayList<>();
+        
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= N; j++){
+                int cur = toCoord(i, j);
+                // 자신의 그룹인 경우
+                if(find(cur) == root){
+                    member.add(cur);
+                }
+            }
+        }
+        
+        // 멤버들 null로 만들기!
+        for(int m : member){
             union[m] = m;
             value[m] = null;
         }
-
-        value[idx] = keep;
-    }
-
-    static String print(String[] arr) {
-        int r = Integer.parseInt(arr[1]);
-        int c = Integer.parseInt(arr[2]);
-        int idx = toCoord(r, c);
-        int rootCoord = find(idx);
-        return (value[rootCoord] == null) ? "EMPTY" : value[rootCoord];
+        
+        value[idx] = temp;
+        
+        return;
     }
     
+    private String print(String[] cmdArr){
+        int r = Integer.parseInt(cmdArr[1]);
+        int c = Integer.parseInt(cmdArr[2]);
+        
+        int idx = toCoord(r, c);
+        
+        int root = find(idx);
+        
+        return value[root];
+    }
 }
