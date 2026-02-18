c2458ac005f12fa360969a101bb1120bb4531dde
Cho-Hyun-Seung <105296338+Cho-Hyun-Seung@users.noreply.github.com>
2026-02-18 15:41:29 +0900

    [Silver I] Title: 그림, Time: 336 ms, Memory: 45276 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -2,67 +2,64 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
-    static int[][] paper;
-    static boolean[][] visited;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    private static int N, M;
+    private static int[][] arr;
+    private static boolean[][] visited;
+    
+    private static int[] dx = {-1, 1, 0, 0};
+    private static int[] dy = {0, 0, 1, -1};
     
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(
-            new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
         
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         
-        paper = new int[N][M];
+        arr = new int[N][M];
         visited = new boolean[N][M];
-        int maxLength = 0;
-        int paperCount = 0;
+        int max = 0;
+        int count = 0;
         
         for(int i = 0; i < N; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < M; j++){
-                paper[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         
-        for(int i = 0; i < N; i++){
+        for(int i = 0; i < N ; i++){
             for(int j = 0; j < M; j++){
-                if(!visited[i][j] && paper[i][j] == 1){
-                    int length = dfs(i, j);
-                    maxLength = Math.max(maxLength, length);
-                    paperCount++;
+                if(!visited[i][j] && arr[i][j] == 1){
+                    count += 1;
+                    max = Math.max(max, getSize(j, i));
                 }
             }
         }
-        
-        sb.append(paperCount).append("\n")
-            .append(maxLength).append("\n");
-        
-        System.out.println(sb.toString());
+        System.out.println(count + "\n" + max);
     }
-    static int dfs(int x, int y){
-        Stack<int[]> stack = new Stack<>();
-        stack.push(new int[]{x, y});
-        visited[x][y] = true;
-        int count = 1;
-        
-        while(!stack.isEmpty()){
-            int[] arr = stack.pop();
+    
+    private static int getSize(int x, int y){
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{y, x});
+        visited[y][x] = true;
+        int size = 0;
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            size += 1;
+            
             for(int i = 0; i < 4; i++){
-                int nx = arr[0] + dx[i];
-                int ny = arr[1] + dy[i];
-                if(nx >=0 && nx < paper.length 
-                   && ny >=0 && ny <paper[0].length
-                  && !visited[nx][ny] && paper[nx][ny] == 1){
-                    visited[nx][ny] = true;
-                    count++;
-                    stack.push(new int[]{nx, ny});
+                int nx = cur[1] + dx[i];
+                int ny = cur[0] + dy[i];
+                
+                if(nx >= 0 && nx < M && ny >= 0 && ny < N && !visited[ny][nx]
+                  && arr[ny][nx] == 1){
+                    q.offer(new int[]{ny, nx});
+                    visited[ny][nx] = true;
                 }
             }
         }
-        return count;
+        
+        return size;
     }
 }
